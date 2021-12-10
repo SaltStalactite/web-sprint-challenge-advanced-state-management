53d238180a820332a1b59f34265ee89a7b1c4ed7
cc <votaryofturms@icloud.com>
2021-12-10 18:32:54 -0500

    connected AddForm component to state and refactored code, changing errors and adding to handleSubmit function

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,60 +1,66 @@
 import React, { useState } from 'react';
-
+import { connect } from 'react-redux'
+import { addSmurf, fetchError } from '../actions';
 const AddForm = (props) => {
     const [state, setState] = useState({
-        name:"",
-        position:"",
-        nickname:"",
-        description:""
+        name: "",
+        position: "",
+        nickname: "",
+        description: ""
     });
+    const { fetchError, addSmurf } = props
 
-    //remove when error state is added
-    const errorMessage = "";
 
     const handleChange = e => {
         setState({
             ...state,
-            [e.target.name]:e.target.value
+            [e.target.name]: e.target.value
         });
     }
 
     const handleSubmit = e => {
         e.preventDefault();
         if (state.name === "" || state.position === "" || state.nickname === "") {
-            //dispatch a custom error action
+            fetchError('All fields required')
         } else {
-            //dispatch an addSmurf action
+            addSmurf(state)
         }
     }
 
-    return(<section>
+    return (<section>
         <h2>Add Smurf</h2>
         <form onSubmit={handleSubmit}>
             <div className="form-group">
-                <label htmlFor="name">Name:</label><br/>
+                <label htmlFor="name">Name:</label><br />
                 <input onChange={handleChange} value={state.name} name="name" id="name" />
             </div>
             <div className="form-group">
-                <label htmlFor="position">Position:</label><br/>
+                <label htmlFor="position">Position:</label><br />
                 <input onChange={handleChange} value={state.position} name="position" id="position" />
             </div>
             <div className="form-group">
-                <label htmlFor="nickname">Nickname:</label><br/>
+                <label htmlFor="nickname">Nickname:</label><br />
                 <input onChange={handleChange} value={state.nickname} name="nickname" id="nickname" />
             </div>
             <div className="form-group">
-                <label htmlFor="description">Description:</label><br/>
+                <label htmlFor="description">Description:</label><br />
                 <textarea onChange={handleChange} value={state.description} name="description" id="description" />
             </div>
             {
-                errorMessage && <div data-testid="errorAlert" className="alert alert-danger" role="alert">Error: {errorMessage}</div>
+                props.errorMes && <div data-testid="errorAlert" className="alert alert-danger" role="alert">Error: {props.errorMes}</div>
             }
             <button>Submit Smurf</button>
         </form>
     </section>);
 }
 
-export default AddForm;
+const mapStateToProps = state => {
+    return {
+        errorMes: state.error
+    }
+}
+
+export default connect(mapStateToProps, { addSmurf, fetchError })(AddForm);
 
 //Task List:
 //1. Connect the errorMessage, setError and addSmurf actions to the AddForm component.
--- src/components/SmurfList.js
+++ src/components/SmurfList.js
@@ -3,13 +3,6 @@ import Smurf from './Smurf';
 import { connect } from 'react-redux'
 const SmurfList = (props) => {
     const { isLoading, smurfs } = props;
-    // const testSmurf = {
-    //     id:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
-    //     name:'Poppa Smurf',
-    //     position:'Village Leader',
-    //     nickname: 'Pops',
-    //     description: 'Papa is the practical village leader and the father figure of 100 or so young Smurfs. He is easily identified by his red Smurf hat, pants, and a shortly-trimmed white beard and moustache.'
-    // }
 
     if (isLoading) {
         return <h1>Loading...</h1>;
@@ -26,7 +19,6 @@ const mapStateToProps = state => {
     return {
         smurfs: state.smurfs,
         isLoading: state.isLoading,
-        error: state.error
     }
 }
 
